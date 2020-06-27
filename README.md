f257c667621dbcc0a1bb2759d4fe852e5a1c63a6
zeze.li <lizeze.boy@gmail.com>
2020-06-27 13:04:42 +0800

    添加icon和iconPosition属性

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vue-gulu
+
+##  内联元素上下不对齐
+```css
+        vertical-align: middle;
+
+```
--- src/button.vue
+++ src/button.vue
@@ -1,21 +1,57 @@
 <template>
-    <div>
-        <button class="g-button">按钮</button>
-    </div>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+
+        </div>
+    </button>
 </template>
 
 <script>
-    export default {name: 'button'}
+    export default {
+        name: 'button',
+        props: ["icon", "iconPosition"]
+    }
 </script>
 
 <style scoped lang="scss">
     .g-button {
         font-size: var(--button-font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 1.1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        vertical-align: middle;
+
+        > .content {
+            order: 2;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+        }
+
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+
+
+            }
+        }
 
         &:hover {
             border-color: var(--border-color-hover);
