493607a6556994e4df38f1c857c0764a8be89f07
zeze.li <lizeze.boy@gmail.com>
2020-07-05 15:11:33 +0800

    增加input组件

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import GButtonGroup from './g-button-group'
-
+ import GInput from './input'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', GButtonGroup)
+Vue.component('g-input', GInput)
 
 new Vue({
     el: '#app',
---
+++ src/input.vue
@@ -0,0 +1,13 @@
+<template>
+    <input type="text">
+</template>
+
+<script>
+    export default {
+        name: "input"
+    }
+</script>
+
+<style  lang="scss">
+
+</style>
